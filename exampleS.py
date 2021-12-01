1c819b7c7f2ae28091c2d0d6700e03f6ec9364d4
Cyril-Meyer <contact@cyrilmeyer.eu>
2021-12-01 16:04:42 +0100

    add gen_click_fill for generatorS

--- exampleS.py
+++ exampleS.py
@@ -83,6 +83,42 @@ def example_3d_extreme(export=False):
     utils.show_stack(utils.combine_image_and_maps(dense_label_3d, pos_click_map, np.zeros(dense_label_3d.shape)), save_filename=save_filename)
 
 
+def example_2d_fill(export=False):
+    dense_label_2d = np.zeros((256, 256), dtype=np.uint8)
+    dense_label_2d[skimage.draw.disk((128, 128), 64)] = 1
+    print(dense_label_2d.shape, dense_label_2d.min(), dense_label_2d.max(), dense_label_2d.dtype)
+    # plt.imshow(dense_label_2d, cmap="gray")
+    # plt.show()
+
+    gen_pos = generatorS.gen_click_fill(dense_label_2d, d_step=40, d_margin=20)
+
+    pos_click_map, _ = next(gen_pos)
+
+    print(pos_click_map.shape, pos_click_map.min(), pos_click_map.max(), pos_click_map.dtype)
+
+    plt.imshow(utils.combine_image_and_maps(dense_label_2d, pos_click_map, np.zeros(dense_label_2d.shape)))
+    if export:
+        plt.savefig('media/example_2d_s_fill.png')
+    plt.show()
+
+
+def example_3d_fill(export=False):
+    dense_label_3d = skimage.draw.ellipsoid(64, 64, 64)
+    print(dense_label_3d.shape, dense_label_3d.min(), dense_label_3d.max(), dense_label_3d.dtype)
+    # utils.show_stack(dense_label_3d, cmap="gray")
+
+    gen_pos = generatorS.gen_click_fill(dense_label_3d, d_step=50, d_margin=15)
+
+    pos_click_map, _ = next(gen_pos)
+
+    print(pos_click_map.shape, pos_click_map.min(), pos_click_map.max(), pos_click_map.dtype)
+
+    save_filename = None
+    if export:
+        save_filename = 'media/example_3d_s_fill.gif'
+    utils.show_stack(utils.combine_image_and_maps(dense_label_3d, pos_click_map, np.zeros(dense_label_3d.shape)), save_filename=save_filename)
+
+
 def benchmark_(dense_label_3d):
     print(dense_label_3d.shape, dense_label_3d.min(), dense_label_3d.max(), dense_label_3d.dtype,
           np.sum(dense_label_3d > 0) / np.prod(dense_label_3d.shape))
@@ -90,6 +126,7 @@ def benchmark_(dense_label_3d):
     gen_pos = generatorS.gen_click_random_uniform(dense_label_3d, click=10)
     gen_pos_adv = generatorS.gen_click_random_uniform_advanced(dense_label_3d, click=10)
     gen_neg = generatorS.gen_click_around_border(dense_label_3d, click=10)
+    gen_pos_fill = generatorS.gen_click_fill(dense_label_3d)
 
     t0 = time.time()
     for _ in range(32):
@@ -110,6 +147,12 @@ def benchmark_(dense_label_3d):
     print('| gen_click_around_border |', round(t1 - t0, 3), '|')
 
     t0 = time.time()
+    for _ in range(32):
+        _, _ = next(gen_pos_fill)
+    t1 = time.time()
+    print('| gen_click_fill |', round(t1 - t0, 3), '|')
+
+    t0 = time.time()
     _, _ = generatorS.get_click_extreme_points(dense_label_3d)
     t1 = time.time()
     print('| get_click_extreme_points |', round(t1 - t0, 3), '|')
@@ -128,9 +171,11 @@ def benchmark():
 
 
 if __name__ == "__main__":
-    example_2d(export=True)
-    example_3d(export=True)
-    example_2d_extreme(export=True)
-    example_3d_extreme(export=True)
-    benchmark()
+    # example_2d(export=True)
+    # example_3d(export=True)
+    # example_2d_extreme(export=True)
+    # example_3d_extreme(export=True)
+    # example_2d_fill(export=True)
+    # example_3d_fill(export=True)
+    # benchmark()
     exit(0)
--- generatorS.py
+++ generatorS.py
@@ -156,3 +156,38 @@ def get_click_extreme_points(label):
         add_click(tuple([zmax, ymax, xmax]))
 
     return click_map, click_pos
+
+
+# generate random click to fill the label.
+# d_step   : minimum distance between different clicks
+# d_margin : minimum distance to the border
+def gen_click_fill(label, label_dt=None, d_step=50, d_margin=20):
+    assert d_step > 0
+
+    if label_dt is None:
+        label_dt = edt.edt(label > 0)
+
+    if d_margin is not None:
+        label_dt = label_dt > d_margin
+
+    label_coord = np.argwhere(label_dt)
+
+    while True:
+        label_dt_ = np.copy(label_dt)
+        label_coord_ = np.copy(label_coord)
+
+        click_map = np.zeros(label.shape, dtype=label.dtype)
+        click_pos = []
+
+        while True:
+            if len(label_coord_) == 0:
+                break
+
+            coord = tuple(label_coord_[random.randint(0, len(label_coord_)-1)])
+            click_map[coord] = 1
+            click_pos.append(coord)
+
+            distances = np.sqrt(np.sum(np.square(np.array(coord) - label_coord_), axis=1))
+            label_coord_ = label_coord_[np.where(distances > d_step)]
+
+        yield click_map, click_pos
